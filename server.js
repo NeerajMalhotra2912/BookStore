59cedb819788709dcb11169f5c2e462bc593b48c
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-12 10:01:45 +0530

    feat : created server file for book store app

---
+++ server.js
@@ -0,0 +1,38 @@
+/**
+ * @description : Express.js is a back end web application framework for Node.js
+ * which can be used for writing business logic.
+ */
+const express = require('express');
+const logger = require('./logger/logger');
+
+const port = process.env.PORT;
+/**
+ * create express app
+ */
+const app = express();
+/**
+ * parse requests of content-type - application/x-www-form-urlencoded
+ * use is a way to register middleware or chain of middlewares before executing any end route logic.
+ */
+app.use(express.json());
+/**
+ * parse requests of content-type - application/json
+ */
+app.use(express.json());
+/**
+ * define a simple route
+ */
+app.get('/', (req, res) => {
+    res.json({ 'message': 'Welcome to Book Store App.' });
+});
+/**
+ * Require Notes routes
+ */
+require('./app/routes/user')(app);
+
+/**
+ * listen for requests
+ */
+app.listen(port, () => { console.log('info', 'Server is listening on port 3000'); });
+
+module.exports = app;
