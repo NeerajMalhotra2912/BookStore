f30a9ca7ae91a46b588bebcb66122148f50f704a
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-17 10:07:22 +0530

    feat : added forget password and reset password api

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -12,6 +12,7 @@
 
 const userService = require('../services/user.js');
 const helper = require('../../helper/validationSchema');
+const jwt = require('jsonwebtoken');
 
 class UserRegistration {
     /**
@@ -100,6 +101,65 @@ class UserRegistration {
                 message: 'Internal error from the server',
             });
         }
+    }
+    forgetPassword = (req, res) => {
+        try {
+            const userData = {
+                email: req.body.email,
+            };
+            userService.forgetPassword(userData, (error, result) => {
+                if (error) {
+                    return res.status(400).send({
+                        success: false,
+                        message: 'Unable to send mail',
+                        error,
+                    });
+                }
+                return res.status(200).send({
+                    success: true,
+                    message: 'Email sent successfully',
+                    result,
+                });
+            });
+        } catch (err) {
+            return res.status(500).send({
+                success: false,
+                message: 'Internal error from the server',
+            });
+        }
+    }
+
+    resetPassword = (req, res) => {
+        try {
+            const verifyRole = jwt.verify(req.headers.token, process.env.JWT);
+            console.log("Role ", verifyRole);
+            const resetPass = {
+                password: req.body.password,
+                email: verifyRole.name,
+            }
+            userService.resetPassword(resetPass, (err, result) => {
+                if (err) {
+                    return res.status(401).send({
+                        success: false,
+                        message: 'Un-authorized access to reset your password',
+                        err,
+                    });
+                } else {
+                    return res.status(200).send({
+                        success: true,
+                        message: 'Password reset is Successful',
+                        // result,
+                    });
+                }
+            });
+        } catch (error) {
+            return res.status(400).send({
+                success: false,
+                message: 'Time-out, please try again to reset your password',
+                error
+            });
+        }
+
     }
 };
 
