73b2dcfede923862ae80462db388fae3093c4163
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-20 10:49:18 +0530

    feat : added getallBooks and update book api

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -28,7 +28,6 @@ class BookController {
                 price: req.body.price,
                 description: req.body.description,
             };
-            console.log("books are : ", bookDetails);
             const validateResult = helper.bookValidationSchema.validate(bookDetails);
             if (validateResult.error) {
                 res.status(400).send({
@@ -57,6 +56,69 @@ class BookController {
                 success: false,
                 message: 'Internal error from the server',
                 err,
+            });
+        }
+    }
+
+    updateBook = (req, res) => {
+        try {
+            const bookDetails = {
+                author: req.body.author,
+                title: req.body.title,
+                image: req.body.image,
+                quantity: req.body.quantity,
+                price: req.body.price,
+                description: req.body.description,
+                bookId: req.params.bookId,
+            };
+            const validateResult = helper.updatedBookSchema.validate(bookDetails);
+            if (validateResult.error) {
+                res.status(400).send({
+                    success: false,
+                    message: 'Please fill all the fields as per rules',
+                    data: validateResult,
+                });
+                return;
+            }
+            const result = bookService.updateBook(bookDetails);
+            result.then(() => {
+                return res.status(200).send({
+                    success: true,
+                    message: 'Your book updated successfully',
+                });
+            }).catch(() => {
+                return res.status(400).send({
+                    success: false,
+                    message: 'Failed to update book',
+                });
+            })
+        } catch (err) {
+            res.status(500).send({
+                success: false,
+                message: 'Internal error from the server',
+            });
+        }
+    }
+
+    getAllBooks = (req, res) => {
+        try {
+            const result = bookService.getAllBooks()
+            result.then((book) => {
+                return res.status(200).send({
+                    success: true,
+                    message: 'Books retrieved successfully',
+                    book,
+                });
+            }).catch((error) => {
+                return res.status(400).send({
+                    success: false,
+                    message: 'Failed to retrieve the books',
+                });
+            })
+        } catch (err) {
+            res.status(500).send({
+                success: false,
+                message: 'Internal error from the server',
             });
         }
     }
