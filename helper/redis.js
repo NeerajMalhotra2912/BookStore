c7b6c68396e3429781ea2ab4ab6d21baf8fb7b8d
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-20 11:26:56 +0530

    refactor : refacotr the redis file and add comments

--- helper/redis.js
+++ helper/redis.js
@@ -1,13 +1,24 @@
 /* eslint-disable indent */
+/**
+ * @description : creating redis cache file for redisCache
+ */
 const redis = require('redis');
 const { addBook } = require('../app/services/book');
 
 const client = redis.createClient();
 
 class Redis {
+
     redisFunction = (KEY, value) => {
         client.setex(KEY, 1200, JSON.stringify(value));
     };
+    /**
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     * @description : redisMiddleware will take the request in key value pair to process it.
+     */
     redisMiddleWare = (req, res, next) => {
         client.get('book', (err, book) => {
             if (err) {
