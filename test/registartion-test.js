ba1893fdc7eb5173a83426f54c8fc93b90b55d3c
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-07-22 10:39:49 +0530

    feat : added test case for user registration

--- test/registartion-test.js
+++ test/registartion-test.js
@@ -21,20 +21,119 @@ const userData = require('./user.json');
 
 chai.should();
 
-describe('registrartion', () => {
-  it('givenUserDetails_whenProper_ShouldRegisterUser', (done) => {
-    const userInfo = userData.user.correct_Data_For_Ragistration;
-    chai.request(server).post('/userRagistrartion').send(userInfo).end((err, res) => {
-      res.should.have.status(404);
-      done();
-    });
+describe('user registartion', () => {
+  it.skip('givenUserDetails_whenProper_ShouldRegisterUser', (done) => {
+    chai.request(server).post('/user')
+      .send(userData.user.userRegistration)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('givenUserDetails_whenWrong_ShouldNotRegisterUser', (done) => {
+    chai.request(server).post('/user')
+      .send(userData.user.in_correct_Registration)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
+
+describe('admin registartion', () => {
+  it.skip('givenAdminDetails_whenProper_shouldRegisterAdmin', (done) => {
+    chai.request(server).post('/admin')
+      .send(userData.user.adminRegistration)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('givenAdminDetails_whenWrong_ShouldNotRegisterAdmin', (done) => {
+    chai.request(server).post('/admin')
+      .send(userData.user.in_correct_Registration)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
+
+describe('user login', () => {
+  it('givenUserLoginDetails_whenProper_shouldDoUserLogin', (done) => {
+    chai.request(server).post('/userLogin')
+      .send(userData.user.userLogin)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('givenUserLoginDetails_whenWrong_shouldNotDoUserLogin', (done) => {
+    chai.request(server).post('/userLogin')
+      .send(userData.user.userLoginWithImproperDetail)
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    done();
+  });
+});
+
+describe('admin login', () => {
+  it('givenAdminLoginDetails_whenProper_shouldDoAdminLogin', (done) => {
+    chai.request(server).post('/adminLogin')
+      .send(userData.user.adminLogin)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('givenAdminLoginDetails_whenWrong_shouldNotDoAdminLogin', (done) => {
+    chai.request(server).post('/adminLogin')
+      .send(userData.user.adminLoginWithImproperDetails)
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    done();
+  });
+});
+
+describe('forget Password', () => {
+  it('givenEmail_whenProper_shouldSendResetMail', (done) => {
+    chai.request(server).post('/forgetPassword')
+      .send(userData.user.forgetPasswordData)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('givenEmail_whenWrong_shouldNotSendResetMail', (done) => {
+    chai.request(server).post('/forgetPassword')
+      .send(userData.user.forgetPasswordWithImproperDetails)
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    done();
+  });
+});
+
+describe('reset Password', () => {
+  it('givenToken_whenWrong_shouldNotResetPassword', (done) => {
+    chai.request(server).post('/resetPassword')
+      .set('token', `${userData.user.credentials.wrongToken}`)
+      .send(userData.user.resetPassword)
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
   });
 
-  it('givenUserDetails_whenWrong_ShouldNotRegisterUser', (done) => {
-    const userInfo = userData.user.in_correct_Data_For_Ragistration;
-    chai.request(server).post('/inCorrectUser').send(userInfo).end((err, res) => {
-      res.should.have.status(404);
-      done();
-    });
+  it('givenToken_whenProper_shouldResetPassword', (done) => {
+    chai.request(server).post('/resetPassword')
+      .set('token', `${userData.user.credentials.token}`)
+      .send(userData.user.resetPassword)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
 });
